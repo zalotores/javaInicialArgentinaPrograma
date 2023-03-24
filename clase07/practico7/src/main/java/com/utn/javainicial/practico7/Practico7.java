ffb8e04846e687d14fb0dda28eda46389b7ea255
zalotores <zalotores@gmail.com>
2023-03-24 10:47:09 -0300

    inicio practico 7

---
+++ clase07/practico7/src/main/java/com/utn/javainicial/practico7/Practico7.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.utn.javainicial.practico7;
+
+/**
+ *
+ * @author gonza
+ */
+public class Practico7 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("**************");
+        System.out.println("Herencia");
+        
+        System.out.println("\n1. Al programa del carrito de la Clase 5, agregue un parámetro que indique si hay un\n" +
+"descuento fijo o % y ponga el valor del mismo.");
+        System.out.println("2. Agregue un nuevo descuento %, pero que tenga un tope Fijo en una nueva clase\n" +
+"DescuentoPorcentajeConTope");
+        System.out.println("3. Cree 2 o 3 Tests para el método “precio” del carrito, y para los 3 descuentos\n" +
+"DescuentoFijo, DescuentoPorcentaje y DescuentoPorcentajeConTope");
+        System.out.println("4. Suba el nuevo código a GIT");
+        
+    }
+}
